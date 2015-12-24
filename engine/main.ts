63a7c45e6d730b4e2dab3c8e9d732ea5db3769da
William DA SILVA <william.da.silva@outlook.com>
2015-12-24 19:59:16 +0100

    Starting a repo for the stand-alone Dickbutt engine

---
+++ engine/main.ts
@@ -0,0 +1,35 @@
+/// <reference path="p2.d.ts" />
+/// <reference path="tween.d.ts" />
+
+/// <reference path="global.ts" />
+/// <reference path="events.ts" />
+/// <reference path="elements.ts" />
+/// <reference path="scene.ts" />
+/// <reference path="camera.ts" />
+/// <reference path="input.ts" />
+/// <reference path="update.ts" />
+
+/// <reference path="render.ts" />
+/// <reference path="render/layer.ts" />
+/// <reference path="render/texture.ts" />
+/// <reference path="render/drawable.ts" />
+/// <reference path="render/sprite.ts" />
+/// <reference path="render/draw/draw.ts" />
+/// <reference path="render/draw/rectangle.ts" />
+/// <reference path="render/draw/circle.ts" />
+/// <reference path="render/draw/polygon.ts" />
+/// <reference path="render/draw/line.ts" />
+/// <reference path="render/draw/text.ts" />
+/// <reference path="render/draw/point.ts" />
+
+/// <reference path="grid.ts" />
+/// <reference path="interface.ts" />
+/// <reference path="ui/gui.ts" />
+/// <reference path="ui/window.ts" />
+/// <reference path="ui/label.ts" />
+/// <reference path="ui/button.ts" />
+/// <reference path="ui/field.ts" />
+/// <reference path="ui/checkbox.ts" />
+
+/// <reference path="sounds.ts" />
+/// <reference path="fonts.ts" />
